ab62e73fbe597dea6b81af51a5165e698b9155d4
johnstclair <167724101+johnstclair@users.noreply.github.com>
2024-11-07 19:13:59 -0800

    feat: new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,96 +9,109 @@ import armImg from "/src/assets/arm.png";
 import lyricSearch from "/src/assets/extension-icon.png";
 import geniusLyricsImg from "/src/assets/geniuslyrics.png";
 import btsmPath from "/src/assets/main.png";
+import nixImg from "/src/assets/nix.png";
 
 function Projects() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const redirectSite = (page) => {
-    navigate(page);
-  };
+	const redirectSite = (page) => {
+		navigate(page);
+	};
 
-  return (
-    <>
-      <Navbar />
-      <div className="projects section" id="projects">
-        <h1>Projects - Images link to project</h1>
-        <div className="tileSection">
-          <Project
-            click={() => redirectSite("/")}
-            reference={"https://github.com/johnstclair/TAADDA_CPU"}
-            useReference={true}
-            author={"Author"}
-            header={"T.A.A.D.D.A CPU"}
-            paragraph={
-              "One of my more recent and time consuming projects, was building and designing a virtual computer, with an special ARM-like architecture."
-            }
-            imagePath={armImg}
-            imageWidth={"100%"}
-          />
-          <Project
-            click={() => redirectSite("/")}
-            reference={"https://gangagames.itch.io/anzz"}
-            useReference={true}
-            author={"Author"}
-            header={"Anzz"}
-            paragraph={
-              "Anzz is a small, silly project I worked on for school to learn Godot Mono (C#).  It's a simulation where ants utilize pheromones to gather food as efficiently as possible. I've stopped devolpment on it, but I may continue in the future."
-            }
-            imagePath={antsImg}
-            imageWidth={"100%"}
-          />
-          <Project
-            click={() => redirectSite("/")}
-            reference={""}
-            useReference={false}
-            author={"Author"}
-            header={"Personal Portfolio"}
-            paragraph={
-              "This website! I have used my knowledge of Javascript, HTML, CSS and React to create my very own personal portfolio!"
-            }
-            imagePath={htmlImg}
-            imageWidth={"100%"}
-          />
-          <Project
-            click={() => redirectSite("/")}
-            reference={"https://github.com/johnstclair/lyric-search"}
-            useReference={true}
-            author={"Author"}
-            header={"Lyric Search"}
-            paragraph={
-              "An extension for Raycast, an app launcher (an much more) for macOS. Built with Typescript and React this extension allows you to search for songs using lyrics, utilizing the Genius Lyric's API"
-            }
-            imagePath={lyricSearch}
-            imageWidth={"100%"}
-          />
-          <Project
-            click={() => redirectSite("/")}
-            reference={"https://www.raycast.com/tkdkid1000/genius-lyrics"}
-            useReference={true}
-            author={"Contributor"}
-            header={"Genius Lyric Raycast Extension"}
-            paragraph={
-              "I added a command that allows users to search for song titles using lyrics to the existing Genius Lyric's extension for Raycast"
-            }
-            imagePath={geniusLyricsImg}
-            imageWidth={"100%"}
-          />
-          <Project
-            click={() => redirectSite("/")}
-            reference={"https://gangagames.itch.io/built-to-scale-mountains"}
-            useReference={true}
-            author={"Developer"}
-            header={"GMTK2024 Game Jam"}
-            paragraph={
-              "With a team of two newcomers to Git and Godot, I was the main developer in Built To Scale: Moutains, a submission to the GMTK2024 Game Jam!"
-            }
-            imagePath={btsmPath}
-            imageWidth={"100%"}
-          ></Project>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className="projects section" id="projects">
+				<h1>Projects - Images link to project</h1>
+				<div className="tileSection">
+					<Project
+						click={() => redirectSite("/")}
+						reference={"https://github.com/johnstclair/TAADDA_CPU"}
+						useReference={true}
+						author={"Author"}
+						header={"T.A.A.D.D.A CPU"}
+						paragraph={
+							"One of my more recent and time consuming projects, was building and designing a virtual computer, with an special ARM-like architecture."
+						}
+						imagePath={armImg}
+						imageWidth={"100%"}
+					/>
+					<Project
+						click={() => redirectSite("/")}
+						reference={"https://gangagames.itch.io/anzz"}
+						useReference={true}
+						author={"Author"}
+						header={"Anzz"}
+						paragraph={
+							"Anzz is a small, silly project I worked on for school to learn Godot Mono (C#).  It's a simulation where ants utilize pheromones to gather food as efficiently as possible. I've stopped devolpment on it, but I may continue in the future."
+						}
+						imagePath={antsImg}
+						imageWidth={"100%"}
+					/>
+					<Project
+						click={() => redirectSite("/")}
+						reference={""}
+						useReference={false}
+						author={"Author"}
+						header={"Personal Portfolio"}
+						paragraph={
+							"This website! I have used my knowledge of Javascript, HTML, CSS and React to create my very own personal portfolio!"
+						}
+						imagePath={htmlImg}
+						imageWidth={"100%"}
+					/>
+					<Project
+						click={() => redirectSite("/")}
+						reference={"https://github.com/johnstclair/lyric-search"}
+						useReference={true}
+						author={"Author"}
+						header={"Lyric Search"}
+						paragraph={
+							"An extension for Raycast, an app launcher (an much more) for macOS. Built with Typescript and React this extension allows you to search for songs using lyrics, utilizing the Genius Lyric's API"
+						}
+						imagePath={lyricSearch}
+						imageWidth={"100%"}
+					/>
+					<Project
+						click={() => redirectSite("/")}
+						reference={"https://www.raycast.com/tkdkid1000/genius-lyrics"}
+						useReference={true}
+						author={"Contributor"}
+						header={"Genius Lyric Raycast Extension"}
+						paragraph={
+							"I added a command that allows users to search for song titles using lyrics to the existing Genius Lyric's extension for Raycast"
+						}
+						imagePath={geniusLyricsImg}
+						imageWidth={"100%"}
+					/>
+					<Project
+						click={() => redirectSite("/")}
+						reference={"https://gangagames.itch.io/built-to-scale-mountains"}
+						useReference={true}
+						author={"Developer"}
+						header={"GMTK2024 Game Jam"}
+						paragraph={
+							"With a team of two newcomers to Git and Godot, I was the main developer in Built To Scale: Moutains, a submission to the GMTK2024 Game Jam!"
+						}
+						imagePath={btsmPath}
+						imageWidth={"100%"}
+					></Project>
+					<Project
+						click={() => redirectSite("/")}
+						reference={"https://gangagames.itch.io/built-to-scale-mountains"}
+						useReference={true}
+						author={"Developer"}
+						header={"NixOS Configuration"}
+						paragraph={
+							"My NixOS configuration, designed from the ground up to be modular, maintainable, reproducable, and stunning."
+						}
+						imagePath={nixImg}
+						imageWidth={"100%"}
+					></Project>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Projects;
