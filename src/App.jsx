fe82bbd11ae5e6f5edfc70e7496df20540bcffb3
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-02-23 22:09:10 -0800

    ci 404

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import Projects from "./components/Projects";
 import "./index.css";
 import "./style.css";
 
+import React from "react";
 import {
     BrowserRouter as Router,
     Routes,
+    useNavigate,
     Route,
 } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  
+  React.useEffect(() => {
+    navigate('/'); 
+  }, [navigate]);
+
+  return ( <></> );
+};
 
 function App() {
   return (
@@ -16,6 +28,7 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/projects" element={<Projects />} />
+              <Route path="*" element={<NotFound />} />
           </Routes>
       </Router>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,7 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
 
-const rewriteSlashToIndexHtml = () => {
-  return {
-    name: 'rewrite-slash-to-index-html',
-    apply: 'serve',
-    enforce: 'post',
-    configureServer(server) {
-      // rewrite / as index.html
-      server.middlewares.use('/', (req, _, next) => {
-        if (req.url === '/') {
-          req.url = '/index.html'
-        }
-        next()
-      })
-    },
-  }
-}
-
-const projects = () => {
-  return {
-    name: 'projects',
-    apply: 'serve',
-    enforce: 'post',
-    configureServer(server) {
-      server.middlewares.use('/', (req, _, next) => {
-        if (req.url === '/projects') {
-          req.url = '/index.html'
-        }
-        next()
-      })
-    },
-  }
-}
-
 export default defineConfig({
-  appType: 'mpa', // disable history fallback
-  plugins: [
-    react(),
-    rewriteSlashToIndexHtml(),
-    projects()
-  ],
+  plugins: [react()],
 })
 
