2320c6499002908350abcfef6161e65460fe136f
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-02-23 21:27:22 -0800

    ci 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Projects from "./components/Projects";
@@ -8,22 +7,11 @@ import Projects from "./components/Projects";
 import "./index.css";
 import "./style.css";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/projects",
-      element: <Projects />,
-    },
-  ],
-  { basename: "/" }
-);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  </BrowserRouter>
 );
