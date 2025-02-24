9d8bb8b50142e4540b841e7525b223179d395ab0
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-02-23 21:46:37 -0800

    ci 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+
 import "./index.css";
 import "./style.css";
+
 import {
     BrowserRouter as Router,
     Routes,
