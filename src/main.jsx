7724dd043ae5ee6522788a9823aef66e0c92f0da
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-04-22 21:24:32 -0700

    fix: blank

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+
+import "./index.css";
+import "./style.css";
+
+import {
+  Route,
+  Routes,
+  HashRouter,
+  Navigate
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/*" element={<Navigate to="/" />}  />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 );
